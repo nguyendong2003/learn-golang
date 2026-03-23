792b5afba511e6b5063809336816f97294d7c446
rs-dongnt1 <dongnt1@runsystem.net>
2026-03-23 18:07:43 +0700

    UPDATE

--- 8-elearning/e-smart-learn/app/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type Config struct {
-	Database Database    `validate:"required" yaml:"database"`
-	JWT      JWTConfig   `validate:"required" yaml:"jwt"`
-	Email    EmailConfig `validate:"required" yaml:"email"`
-	Redis    RedisConfig `validate:"required" yaml:"redis"`
-	Frontend string      `validate:"required" yaml:"frontend"`
-}
--- 8-elearning/e-smart-learn/app/config/database.go
+++
@@ -1,5 +0,0 @@
-package config
-
-type Database struct {
-	Dsn string `validate:"required"`
-}
